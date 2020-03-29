e060f55f0ec551921d21f78cdcd87246394f5186
pbeniwal <pbeniwal>
2020-03-29 22:30:29 +0530

    fixed

--- src/main/java/com/edureka/HelloWebApp.java
+++ src/main/java/com/edureka/HelloWebApp.java
@@ -17,7 +17,7 @@ public class HelloWebApp extends HttpServlet {
 
 
         public int add(int a, int b) {
-            return a + b + 1;
+            return a + b ;
         }
 
 	public int sub(int a, int b) {
