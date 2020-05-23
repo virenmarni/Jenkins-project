c9380ce0b1f0640c464602c986cdec07283bc04d
pbeni <pbeni@PRASHANT>
2020-05-23 21:07:27 +0530

    updated

--- src/main/java/com/edureka/HelloWebApp.java
+++ src/main/java/com/edureka/HelloWebApp.java
@@ -18,8 +18,8 @@ public class HelloWebApp extends HttpServlet {
         	resp.setContentType("text/html");
         	
         	out.println("<html>");
-        	out.println("<body bgcolor=\"Aqua\">");
-        	//out.println("<body bgcolor=\"Green\">");
+        	//out.println("<body bgcolor=\"Aqua\">");
+        	out.println("<body bgcolor=\"Green\">");
         	out.println("<h1>Hello from Edureka</h1>");
         	
         	out.println("<h3>Demo Java Project for Edureka DevOps certification May-2020 from Prashant Beniwal</h3>");
